57f289dc18808a2afc908eb2906250e957a44b25
Alex Chiam <chiamjuinhoong@gmail.com>
2025-12-23 20:48:35 +0800

    fix: configure app.module.ts so that it uses postgres instead of sqlite

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,8 +5,12 @@ import { EventsModule } from './events/events.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'dev.db',
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432'),
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME || 'cookie_itchy',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
